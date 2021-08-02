d3a7e834589264018b97ecdbf83861a49dc1b2a5
staplezz <29287497+staplezz@users.noreply.github.com>
2021-08-02 18:45:19 -0500

    Termina login y registro con mensajes al usuario.

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,24 @@ const passport = require('passport');
 
 // Petición GET para el login de la aplicación.
 router.get('/', function (req, res, next) {
-    res.render('login/login', {title: "Noteras", error: req.flash('error') });
+    // Obtenemos el error si es que hay.
+    error = req.flash('error');
+
+    if (error.length === 0) {
+        message = {
+            show: false
+        }
+        res.render('login/login', {title: "Noteras",
+                                message: message});
+    } else {
+        message = {
+            show: true,
+            isError: true,
+            error: error
+        }
+        res.render('login/login', {title: "Noteras",
+                                message: message});
+    }
 });
 
 // Petición POST para el login de la aplicación.
--- routes/register.js
+++ routes/register.js
@@ -5,24 +5,31 @@ const Usuario = require('../models/usuario');
 const passport = require('passport');
 
 router.get("/", function (req, res) {
-    res.render('register/register');
+
+    error = {
+        show: false
+    }
+
+    res.render('register/register', {error: error});
 })
 
 router.post("/", function (req, res){
     let { nombre, username, password} = req.body;
-    
-    // Sanity check
-    console.log(nombre);
-    console.log(username);
-    console.log(password);
 
     Usuario.register(new Usuario({username: username, nombre: nombre}), password, function(err, user){
         if(err){
             // Regresamos al registro y mostramos el error al usuario.
-            //res.render('register/register', {error: err.message})
+            err.show = true;
+            res.render('register/register', {error: err})
         } else {
             // Enviamos al login con mensaje de éxito.
-            res.render('login/login', {message: '¡Usuario registrado con éxito!'});
+            message = {
+                show: true,
+                isError: false,
+                message: '¡Usuario registrado con éxito!'
+            }
+
+            res.render('login/login', { message });
         }
     })
 })
